a084600138169b67b7d96f3b01cbb6d12a88cae6
Sumeet Turkar <121540275+Codellen@users.noreply.github.com>
2023-04-01 00:13:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age)
+	{
+		this.Age = ""
+		this.Name = getName;
+	}
+	get getName()
+	{
+		this.Name = name;
+	}
+	set setterage(age)
+	{
+		this.Age = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		super(name)
+		console.log(this.name+" is studying")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		super(name)
+		console.log(this.name+" is teaching")
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
